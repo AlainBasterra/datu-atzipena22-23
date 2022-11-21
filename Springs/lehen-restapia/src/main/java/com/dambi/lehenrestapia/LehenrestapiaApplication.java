c9842dbf6099b9810989e4db66443b03f2e07f94
Alain Basterra <basterra.alain@uni.eus>
2022-11-21 13:41:03 +0100

    Spring hobetua

--- Springs/lehen-restapia/src/main/java/com/dambi/lehenrestapia/LehenrestapiaApplication.java
+++ Springs/lehen-restapia/src/main/java/com/dambi/lehenrestapia/LehenrestapiaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Random;
 
 @SpringBootApplication
 @RestController
@@ -13,11 +14,12 @@ public class LehenrestapiaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LehenrestapiaApplication.class, args);
 	}
+
 	@GetMapping("/")
 	public String home() {
-		return "Gure lehen rest apia martxan dago."; 
+		return "Gure lehen rest apia martxan dago.";
 	}
-	
+
 	@GetMapping("/kaixo")
 	public String hello(@RequestParam(value = "izena", defaultValue = "mundua") String izena) {
 		return String.format("Kaixo %s!", izena);
@@ -25,19 +27,24 @@ public class LehenrestapiaApplication {
 
 	@GetMapping("/zenbatu")
 	public String zenbatu(@RequestParam(value = "norarte", defaultValue = "5") String norarte) {
-
+		Random rand = new Random();
 		String strEmaitza = "";
+		strEmaitza += "<body background=\"https://i.kym-cdn.com/photos/images/newsfeed/002/111/316/c57.gif\" style=\"-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;\">";
 		for (int i = 1; i <= Integer.parseInt(norarte); i++) {
-			if ((i%2) == 0){
-				strEmaitza += ("<b>" + i + " </b><label style=\"color:blue; font-size: 40px;\">ඞ</label></br>");
-			} else if ((i%3)== 0 ){
-				strEmaitza += ("<b>" + i + " </b><label style=\"color:green; font-size: 40px;\">ඞ</label></br>");
-			} else if ((i%5)== 0){
-				strEmaitza += ("<b>" + i + " </b><label style=\"color:yellow; font-size: 40px;\">ඞ</label></br>");
+			int r = rand.nextInt(255);
+			int b = rand.nextInt(255);
+			int g = rand.nextInt(255);
+			int n = rand.nextInt(100);
+			n++;
+			if (n >= 1 && n <= 5) {
+				strEmaitza += ("<label style=\"color:red; font-size: 40px;\"><img src=\"https://d2t1xqejof9utc.cloudfront.net/screenshots/pics/d0dc0cd3fcf603c28df5f7874baf63fc/large.png\" width=\"40px\" height=\"40px\"> <a href=\"https://amogus.org/conspiracmogus.html\" style=\"text-decoration: none;\">SUS</a></label>");
+			} else if (n == 6) {
+				strEmaitza += ("<label style=\"color:red; font-size: 40px;\"><img src=\"https://media.tenor.com/eUOiCZiskd8AAAAC/monkey-spinning-holding-hands.gif\" width=\"40px\" height=\"40px\"> <a href=\"https://amogus.org/conspiracmogus.html\" style=\"text-decoration: none;\">SUS</a></label>");
+
 			} else {
-				strEmaitza += ("<b>" + i + " </b><label style=\"color:red; font-size: 40px;\">ඞ SUS</label></br>");
+				strEmaitza += ("<label style=\"color:rgb(" + r + "," + g + "," + b
+						+ "); font-size: 40px;\">ඞ</label>");
 			}
-			
 		}
 		return strEmaitza;
 	}
