b5c3417ce3e22db0b3b6ad4d8042f73f10a8e5c2
Alain Basterra <basterra.alain@uni.eus>
2022-11-23 12:33:01 +0100

    Update GreetingController.java

--- Springs/RestApiakSpringekin/restapiakspringekin/src/main/java/com/dambi/restapiakspringekin/controller/GreetingController.java
+++ Springs/RestApiakSpringekin/restapiakspringekin/src/main/java/com/dambi/restapiakspringekin/controller/GreetingController.java
@@ -16,7 +16,7 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	@RequestMapping("/api")
+	//@RequestMapping("/api")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
