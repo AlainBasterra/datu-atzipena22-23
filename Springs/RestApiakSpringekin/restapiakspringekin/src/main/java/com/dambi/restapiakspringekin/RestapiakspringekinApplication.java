1d45ff22d065a6323a8664127e8de1ddc2df9d43
Alain Basterra <basterra.alain@uni.eus>
2022-11-23 12:24:52 +0100

    Diagrama

--- Springs/RestApiakSpringekin/restapiakspringekin/src/main/java/com/dambi/restapiakspringekin/RestapiakspringekinApplication.java
+++ Springs/RestApiakSpringekin/restapiakspringekin/src/main/java/com/dambi/restapiakspringekin/RestapiakspringekinApplication.java
@@ -1,4 +1,4 @@
-package com.dambi.restapiakspringekin.main_klasea;
+package com.dambi.restapiakspringekin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
