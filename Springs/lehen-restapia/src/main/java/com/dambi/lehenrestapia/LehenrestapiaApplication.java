514f84cab5cdd8de065765fa6faa5b4e93874500
Alain Basterra <basterra.alain@uni.eus>
2022-11-23 12:04:11 +0100

    Rest Apiak Springekin

--- Springs/lehen-restapia/src/main/java/com/dambi/lehenrestapia/LehenrestapiaApplication.java
+++ Springs/lehen-restapia/src/main/java/com/dambi/lehenrestapia/LehenrestapiaApplication.java
@@ -29,7 +29,7 @@ public class LehenrestapiaApplication {
 	public String zenbatu(@RequestParam(value = "norarte", defaultValue = "5") String norarte) {
 		Random rand = new Random();
 		String strEmaitza = "";
-		strEmaitza += "<body background=\"https://i.kym-cdn.com/photos/images/newsfeed/002/111/316/c57.gif\" style=\"-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;\">";
+		//strEmaitza += "<body background=\"https://i.kym-cdn.com/photos/images/newsfeed/002/111/316/c57.gif\" style=\"-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;\">";
 		for (int i = 1; i <= Integer.parseInt(norarte); i++) {
 			int r = rand.nextInt(255);
 			int b = rand.nextInt(255);
@@ -37,9 +37,9 @@ public class LehenrestapiaApplication {
 			int n = rand.nextInt(100);
 			n++;
 			if (n >= 1 && n <= 5) {
-				strEmaitza += ("<label style=\"color:red; font-size: 40px;\"><img src=\"https://d2t1xqejof9utc.cloudfront.net/screenshots/pics/d0dc0cd3fcf603c28df5f7874baf63fc/large.png\" width=\"40px\" height=\"40px\"> <a href=\"https://amogus.org/conspiracmogus.html\" style=\"text-decoration: none;\">SUS</a></label>");
+				strEmaitza += ("<label style=\"color:red; font-size: 40px;\"><a href=\"https://amogus.org/conspiracmogus.html\" style=\"text-decoration: none;\"> <img src=\"https://d2t1xqejof9utc.cloudfront.net/screenshots/pics/d0dc0cd3fcf603c28df5f7874baf63fc/large.png\" width=\"40px\" height=\"40px\"></a></label>");
 			} else if (n == 6) {
-				strEmaitza += ("<label style=\"color:red; font-size: 40px;\"><img src=\"https://media.tenor.com/eUOiCZiskd8AAAAC/monkey-spinning-holding-hands.gif\" width=\"40px\" height=\"40px\"> <a href=\"https://amogus.org/conspiracmogus.html\" style=\"text-decoration: none;\">SUS</a></label>");
+				strEmaitza += ("<label style=\"color:red; font-size: 40px;\"><a href=\"https://amogus.org/conspiracmogus.html\" style=\"text-decoration: none;\"><img src=\"https://media.tenor.com/eUOiCZiskd8AAAAC/monkey-spinning-holding-hands.gif\" width=\"40px\" height=\"40px\"></a></label>");
 
 			} else {
 				strEmaitza += ("<label style=\"color:rgb(" + r + "," + g + "," + b
